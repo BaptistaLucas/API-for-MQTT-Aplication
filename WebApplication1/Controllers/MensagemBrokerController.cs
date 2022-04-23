51c47f9bb06485fbd18b2358db4e75e19ee75f2c
Lucas de O. Baptista <lucasbaptista@univem.edu.br>
2022-04-22 22:18:44 -0300

    second commit

--- WebApplication1/Controllers/MensagemBrokerController.cs
+++ WebApplication1/Controllers/MensagemBrokerController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 using WebApplication1.Models;
+using System.Threading;
+using Microsoft.AspNetCore.SignalR;
+using WebApplication1.Hubteste;
 
 namespace WebApplication1.Controllers
 {
@@ -13,11 +16,20 @@ namespace WebApplication1.Controllers
     [Route("v1/MensagemBroker")]
     public class MensagemBrokerController : ControllerBase
     {
+        private readonly IHubContext<MyHub> hubContext;
+       // List<MensagemBroker> ListaMensagem = new List<MensagemBroker>();
+
+        public MensagemBrokerController(IHubContext<MyHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
         [HttpGet]
         [Route("")]
 
         public async Task<ActionResult<List<MensagemBroker>>> Get([FromServices] DataContext context)
         {
+            Thread.Sleep(1000);
             var mensagens = await context.MensagemBrokers.ToListAsync();
             return mensagens;
         }
@@ -25,14 +37,15 @@ namespace WebApplication1.Controllers
         [HttpPost]
         [Route("")]
 
-        public async Task<ActionResult<MensagemBroker>> Post ([FromServices] DataContext context, [FromBody] MensagemBroker model)
+        public async Task<ActionResult<MensagemBroker>> Post([FromServices] DataContext context, [FromBody] MensagemBroker model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 context.MensagemBrokers.Add(model);
+                await hubContext.Clients.All.SendAsync("post", model);
                 await context.SaveChangesAsync();
                 return model;
-                
+
             }
             else
             {
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,15 +11,19 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using WebApplication1.Data;
+using WebApplication1.Hubteste;
 
 namespace WebApplication1
 {
     public class Startup
     {
+        private readonly Channel<string> _channel;
         public Startup(IConfiguration configuration)
         {
+           // _channel = Channel.CreateUnbounded<string>();
             Configuration = configuration;
         }
 
@@ -27,9 +32,12 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
             services.AddScoped<DataContext, DataContext>();
             services.AddControllers();
+            services.AddSignalR();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,16 +48,17 @@ namespace WebApplication1
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().WithOrigins("http://127.0.0.1:5500", "http://localhost:5500").AllowCredentials());
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<MyHub>("/myHub");
             });
+           
+
         }
     }
 }
